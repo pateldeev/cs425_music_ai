0730e90438ede0b5852b461cf011845e059858d1
pateldeev <pateldeev@nevada.unr.edu>
2021-02-20 14:15:26 -0800

    Update. Add flags. Make model run faster on GPU

--- ml_model/main.py
+++ ml_model/main.py
@@ -1,92 +1,64 @@
+import argparse
 import glob
 import os
 import pickle
 import sys
 
+from midi2audio import FluidSynth
 from music21 import chord, converter, instrument, note, stream
+import musdb
+from pydub import AudioSegment
+import wave
+
 import numpy as np
 import tensorflow as tf
 
-# TODO - Convert these to command line flags.
-USE_GPU = True
-USE_CACHE = True
-TRAIN_NETWORK = False
-TMP_DIR = 'tmp'
+parser = argparse.ArgumentParser(description='Train ML music model')
 
-# Input music params.
-CACHE_FILE = "notes"
-INPUT_MIDI_FILES = glob.glob("input_songs/*.mid")
+# Setup Params.
+parser.add_argument('--no_gpu', action='store_true')
+parser.add_argument('--use_cache', action='store_true')
+parser.add_argument('--no_train', action='store_true')
 
-# Model params.
-LSTM_SEQ_LENGTH = 100
-NUM_EPOCHS = 200
-BATCH_SIZE = 128
-TRAINING_WEIGHTS_FN = "weights_training_E{epoch:04d}_L{loss:.4f}.hdf5"
-FINAL_WEIGHTS_FILE = "weights_trained.hdf5"
-OUTPUT_NOTES = 500
-OUTPUT_FILE = "model_output.mid"
+# Input Params.
+parser.add_argument('--tmp_dir',
+                    type=str,
+                    default='/home/dp/Desktop/cs425_music_ai/ml_model/tmp')
+parser.add_argument('--cache_file', type=str, default='notes_cache')
+parser.add_argument(
+    '--input_files_glob',
+    type=str,
+    default='/home/dp/Desktop/cs425_music_ai/ml_model/input_songs/*.mid')
+parser.add_argument('--input_files_override', type=str, nargs='+')
 
-userLoop = False
+# Training Params
+parser.add_argument('--lstm_seq_len', type=int, default=100)
+parser.add_argument('--num_epochs', type=int, default=100)
+parser.add_argument('--batch_size', type=int, default=128)
 
-# gives user option to change model parameters
-tempInput = input("Do you want to change any model parameters? y/n\n").lower()
-if tempInput == 'y':
-    userLoop = True
+# Output Params.
+parser.add_argument('--final_weights_file',
+                    type=str,
+                    default='weights_trained.hdf5')
+parser.add_argument('--output_notes', type=int, default=50)
+parser.add_argument(
+    '--output_file',
+    type=str,
+    default='/home/dp/Desktop/cs425_music_ai/ml_model/model_output.mid')
 
-# loops until all desired parameters have been changed
-while userLoop:
-    # lists all changeable parameters
-    print("Enter the number of the parameter you want to change")
-    print("1. USE_GPU: " + str(USE_GPU))
-    print("2. USE_CACHE: " + str(USE_CACHE))
-    print("3. TRAIN_NETWORK: " + str(TRAIN_NETWORK))
-    print("4. TMP_DIR: " + str(TMP_DIR))
-    print("5. CACHE_FILE: " + str(CACHE_FILE))
-    print("6. INPUT_MIDI_FILES: " + str(INPUT_MIDI_FILES))
-    print("7. LSTM_SEQ_LENGTH: " + str(LSTM_SEQ_LENGTH))
-    print("8. NUM_EPOCHS: " + str(NUM_EPOCHS))
-    print("9. BATCH_SIZE: " + str(BATCH_SIZE))
-    print("10. TRAINING_WEIGHTS_FN: " + str(TRAINING_WEIGHTS_FN))
-    print("11. FINAL_WEIGHTS_FILE: " + str(FINAL_WEIGHTS_FILE))
-    print("12. OUTPUT_NOTES: " + str(OUTPUT_NOTES))
-    print("13. OUTPUT_FILE: " + str(OUTPUT_FILE))
-    print("14. Exit")
-    paramChange = input()
+args = parser.parse_args()
 
-    # compares user input to possible options, then allows the to change the requested flag
-    if paramChange == '1':
-        USE_GPU = input("Enter the new parameter value: ")
-    elif paramChange == '2':
-        USE_CACHE = input("Enter the new parameter value: ")
-    elif paramChange == '3':
-        TRAIN_NETWORK = input("Enter the new parameter value: ")
-    elif paramChange == '4':
-        TMP_DIR = input("Enter the new parameter value: ")
-    elif paramChange == '5':
-        CACHE_FILE = input("Enter the new parameter value: ")
-    elif paramChange == '6':
-        INPUT_MIDI_FILES += [input("Enter the location of the file you want to add: ")]
-    elif paramChange == '7':
-        LSTM_SEQ_LENGTH = int(input("Enter the new parameter value: "))
-    elif paramChange == '8':
-        NUM_EPOCHS = int(input("Enter the new parameter value: "))
-    elif paramChange == '9':
-        BATCH_SIZE = int(input("Enter the new parameter value: "))
-    elif paramChange == '10':
-        TRAINING_WEIGHTS_FN = input("Enter the new parameter value: ")
-    elif paramChange == '11':
-        FINAL_WEIGHTS_FILE = input("Enter the new parameter value: ")
-    elif paramChange == '12':
-        OUTPUT_NOTES = int(input("Enter the new parameter value: "))
-    elif paramChange == '13':
-        OUTPUT_FILE = input("Enter the new parameter value: ")
-    elif paramChange == '14':
-        userLoop = False
-    else:
-        print("\nInvalid Input, please select another option")
-
-if not USE_GPU:
+# Configure GPU.
+if args.no_gpu:
+    # Don't use GPU.
     os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+else:
+    # Prevent GPU memory overflow.
+    tf.config.experimental.set_memory_growth(
+        tf.config.list_physical_devices('GPU')[0], enable=True)
+
+# Constants.
+TRAINING_WEIGHTS_FN = "weights_training_E{epoch:04d}_L{loss:.4f}.hdf5"
 TF_DEVICES = tf.config.list_physical_devices()
 
 
@@ -94,10 +66,11 @@ def get_notes_from_midi(fns):
     """ Get all the notes and chords from the midi files. """
     notes = []
 
-    for fn in fns:
+    for fn in sorted(fns):
         midi = converter.parse(fn)
 
-        print("Parsing file: ", fn)
+        print("Parsing input file: ", fn)
+        sys.stdout.flush()
 
         try:  # File has instrument parts.
             s2 = instrument.partitionByInstrument(midi)
@@ -121,21 +94,20 @@ def generate_data_sequences(notes, pitches):
     """ Generates the data sequences used by the Neural Network.
     Returns (network inputs raw (unscaled), network inputs, network outputs or labels) """
     # Map between notes and integers.
-    note_to_int = {
-        note: num for num, note in enumerate(pitches)
-    }
+    note_to_int = {note: num for num, note in enumerate(pitches)}
 
     # Create raw inputs and outputs based on sequences.
     net_in_raw, net_out_raw = [], []
-    for i in range(len(notes) - LSTM_SEQ_LENGTH):
-        seq_in = notes[i: i + LSTM_SEQ_LENGTH]
-        seq_out = notes[i + LSTM_SEQ_LENGTH]
+    for i in range(len(notes) - args.lstm_seq_len):
+        seq_in = notes[i:i + args.lstm_seq_len]
+        seq_out = notes[i + args.lstm_seq_len]
 
-        net_in_raw.append(np.array([note_to_int[note] for note in seq_in], dtype=np.float))
+        net_in_raw.append(
+            np.array([note_to_int[note] for note in seq_in], dtype=np.float))
         net_out_raw.append(note_to_int[seq_out])
 
     # Reshape and normalize the input into a format compatible with LSTM layers.
-    net_in = np.reshape(net_in_raw, (len(net_in_raw), LSTM_SEQ_LENGTH, 1))
+    net_in = np.reshape(net_in_raw, (len(net_in_raw), args.lstm_seq_len, 1))
     net_in /= len(pitches)
 
     # Categorize outputs.
@@ -147,13 +119,14 @@ def generate_data_sequences(notes, pitches):
 def create_model(net_in, pitches):
     """ Create the tensorflow LSTM based model. """
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.LSTM(
-        512,
-        input_shape=(net_in.shape[1], net_in.shape[2]),
-        recurrent_dropout=0.3,
-        return_sequences=True
-    ))
-    model.add(tf.keras.layers.LSTM(512, return_sequences=True, recurrent_dropout=0.3))
+    model.add(
+        tf.keras.layers.LSTM(512,
+                             input_shape=(net_in.shape[1], net_in.shape[2]),
+                             recurrent_dropout=0.3,
+                             return_sequences=True))
+    model.add(
+        tf.keras.layers.LSTM(512, return_sequences=True,
+                             recurrent_dropout=0.3))
     model.add(tf.keras.layers.LSTM(512))
     model.add(tf.keras.layers.BatchNormalization())
     model.add(tf.keras.layers.Dropout(0.3))
@@ -172,12 +145,14 @@ def train(model, x, y, batch_size, epochs):
     """ Train the model. """
 
     # Delete any older weights files.
-    for f in glob.glob(os.path.join(TMP_DIR, TRAINING_WEIGHTS_FN[:TRAINING_WEIGHTS_FN.find('{')])):
+    for f in glob.glob(
+            os.path.join(args.tmp_dir,
+                         TRAINING_WEIGHTS_FN[:TRAINING_WEIGHTS_FN.find('{')])):
         os.remove(f)
 
     # Define checkpoint to save weights during training.
     ckpt = tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.path.join(TMP_DIR, TRAINING_WEIGHTS_FN),
+        filepath=os.path.join(args.tmp_dir, TRAINING_WEIGHTS_FN),
         monitor='loss',
         verbose=0,
         save_best_only=True,
@@ -188,22 +163,22 @@ def train(model, x, y, batch_size, epochs):
     model.fit(x=x, y=y, epochs=epochs, batch_size=batch_size, callbacks=[ckpt])
 
 
-def predict(model, network_in_raw, pitches):
+def predict(model, network_in_raw, pitches, num_notes):
     """ Generate notes from the neural network. """
     # Pick a random sequence from the input as a starting point for the prediction
-    start = np.random.randint(0, len(network_in_raw)-1)
+    start = np.random.randint(0, len(network_in_raw) - 1)
 
     # Map between notes and integers.
-    int_to_note = {
-        num: note for num, note in enumerate(pitches)
-    }
+    int_to_note = {num: note for num, note in enumerate(pitches)}
 
     pattern = list(network_in_raw[start])
     model_out = []
 
     # Generate notes.
-    for ni in range(OUTPUT_NOTES):
+    for ni in range(num_notes):
         print("Generating note: ", ni)
+        sys.stdout.flush()
+
         net_in = np.reshape(pattern, (1, len(pattern), 1))
         net_in = net_in / float(len(pitches))
 
@@ -218,8 +193,8 @@ def predict(model, network_in_raw, pitches):
     return model_out
 
 
-def create_midi(prediction_data):
-    """ Convert the output from the prediction to notes and create a midi file from the notes """
+def create_output_notes(prediction_data):
+    """ Creates output notes """
     offset = 0
     output_notes = []
 
@@ -247,41 +222,49 @@ def create_midi(prediction_data):
         # Increase offset each iteration so that notes do not stack
         offset += 0.5
 
-    midi_stream = stream.Stream(output_notes)
-
-    midi_stream.write('midi', fp=OUTPUT_FILE)
-    print("Saved prediciton to: ", OUTPUT_FILE)
+    return output_notes
 
 
 if __name__ == '__main__':
-    print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\nStart")
+    print(
+        "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\nStart"
+    )
 
     # Print system and device information.
     print("Python version: ", sys.version)
     print("Version info:", sys.version_info)
     print("Tensorflow version:", tf.__version__)
     print("Tensorflow Devices:", TF_DEVICES)
+    sys.stdout.flush()
 
     # Create directory for notes cache and weights.
-    if not os.path.exists(TMP_DIR):
-        os.makedirs(TMP_DIR)
+    if not os.path.exists(args.tmp_dir):
+        os.makedirs(args.tmp_dir)
 
     # Get notes.
-    if not USE_CACHE:
-        notes = get_notes_from_midi(fns=INPUT_MIDI_FILES)
-        with open(os.path.join(TMP_DIR, CACHE_FILE), "wb") as f:
-            pickle.dump(notes, f)
-    else:
-        with open(os.path.join(TMP_DIR, CACHE_FILE), "rb") as f:
+    print('||||||||||||||||||||||||||||||||||||||| Getting Notes')
+    if args.use_cache:
+        with open(os.path.join(args.tmp_dir, args.cache_file), "rb") as f:
             notes = pickle.load(f)
+    else:
+        input_files = args.input_files_override if args.input_files_override \
+            else glob.glob(args.input_files_glob)
+        assert input_files, 'No input files!'
+        notes = get_notes_from_midi(fns=input_files)
+
+        # Save to cache.
+        with open(os.path.join(args.tmp_dir, args.cache_file), "wb") as f:
+            pickle.dump(notes, f)
+    print('||||||||||||||||||||||||||||||||||||||| Done Getting Notes')
     # print(notes)
 
-    # Orderd list of unique pitches.
+    # Ordered list of unique pitches.
     pitches = sorted(set(notes))
     # print(pitches)
 
     # Get network inputs and outputs (labels).
-    net_in_raw, net_in, net_out = generate_data_sequences(notes=notes, pitches=pitches)
+    net_in_raw, net_in, net_out = generate_data_sequences(notes=notes,
+                                                          pitches=pitches)
     # print("Network inputs: ", net_in)
     # print("Network ouputs: ", net_out)
 
@@ -290,18 +273,54 @@ if __name__ == '__main__':
     print(model.summary())
 
     # Train model or load weights from file.
-    if TRAIN_NETWORK:
-        train(model=model, x=net_in, y=net_out, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS)
-        model.save_weights(os.path.join(TMP_DIR, FINAL_WEIGHTS_FILE))
+    if not args.no_train:
+        train(model=model,
+              x=net_in,
+              y=net_out,
+              batch_size=args.batch_size,
+              epochs=args.num_epochs)
+        model.save_weights(os.path.join(args.tmp_dir, args.final_weights_file))
     else:
-        model.load_weights(os.path.join(TMP_DIR, FINAL_WEIGHTS_FILE))
+        model.load_weights(os.path.join(args.tmp_dir, args.final_weights_file))
     print("Got trained model!")
+    sys.stdout.flush()
 
     # Generation prediction of model.
-    model_prediction = predict(model=model, network_in_raw=net_in_raw, pitches=pitches)
+    print('||||||||||||||||||||||||||||||||||||||| Making New Song!')
+    model_prediction = predict(model=model,
+                               network_in_raw=net_in_raw,
+                               pitches=pitches,
+                               num_notes=args.output_notes)
+    print('||||||||||||||||||||||||||||||||||||||| Done Making New Song!')
     print("Predicted Notes:", model_prediction)
+    sys.stdout.flush()
 
-    # Save prediction as midi file.
-    create_midi(prediction_data=model_prediction)
+    # Create output notes and save output.
+    print('||||||||||||||||||||||||||||||||||||||| Generating final output!')
+    output_notes = create_output_notes(prediction_data=model_prediction)
 
-    print("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\nEnd")
+    midi_stream = stream.Stream(output_notes)
+    output_file = args.output_file
+    if output_file.endswith('.mid'):
+        midi_stream.write('midi', fp=args.output_file)
+    elif output_file.endswith('.wav'):
+        tmp_output = os.path.join(args.tmp_dir, 'tmp_output.mid')
+        midi_stream.write('midi', fp=tmp_output)
+        fs = FluidSynth()
+        fs.midi_to_audio(tmp_output, output_file)
+    elif output_file.endswith('.mp3'):
+        tmp_output_mid = os.path.join(args.tmp_dir, 'tmp_output.mid')
+        tmp_output_wav = os.path.join(args.tmp_dir, 'tmp_output.wav')
+        midi_stream.write('midi', fp=tmp_output_mid)
+        fs = FluidSynth()
+        fs.midi_to_audio(tmp_output_mid, tmp_output_wav)
+        sound = AudioSegment.from_wav(tmp_output_wav)
+        sound.export(output_file, format="mp3")
+    else:
+        assert False, 'Unsupported output file!'
+    print("Saved prediction to: ", output_file)
+
+    print(
+        "|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\nEnd"
+    )
+    sys.stdout.flush()
